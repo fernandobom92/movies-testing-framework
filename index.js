fb3c96f6e0d6d05e82365c69147a37ae36b23319
fernandobom92 <106743747+fernandobom92@users.noreply.github.com>
2025-03-27 22:29:33 -0300

    Update index.js

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ let rightMovie;
 const onMovieSelect = async (movie, summaryElement, side) => {
   const response = await axios.get('http://www.omdbapi.com/', {
     params: {
-      apikey: 'd9835cc5',
+      apikey: '77b54ebd',
       i: movie.imdbID
     }
   });
